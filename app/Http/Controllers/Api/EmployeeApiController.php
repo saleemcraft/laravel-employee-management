5e46b6ae83eb092dda2ba84fd89b47751b3347fd
saleemcraft <saleemshajahan03@gmail.com>
2026-03-28 02:01:07 +0530

    Added REST API

---
+++ app/Http/Controllers/Api/EmployeeApiController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeApiController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Employee::all());
+    }
+
+    public function store(Request $request)
+    {
+        $employee = Employee::create($request->all());
+        return response()->json($employee, 201);
+    }
+
+    public function show($id)
+    {
+        return response()->json(Employee::findOrFail($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $employee = Employee::findOrFail($id);
+        $employee->update($request->all());
+        return response()->json($employee);
+    }
+
+    public function destroy($id)
+    {
+        Employee::destroy($id);
+        return response()->json(['message' => 'Deleted successfully']);
+    }
+}
